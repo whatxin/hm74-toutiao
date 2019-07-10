74c718f376e55f7aa8a4a465eea93d2ae4e4f4b1
whatxin <2045513674@qq.com>
2019-07-10 20:10:33 +0800

    element-ui

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <template>
   <div id="app">
-
+<el-input-number v-model="num" @change="handleChange" :min="1" :max="10" label="描述文字"></el-input-number>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      num: 1
+    }
+  },
+  methods: {
+    handleChange (value) {
+      console.log(value)
+    }
+  }
 }
+
 </script>
 
 <style lang="less">
