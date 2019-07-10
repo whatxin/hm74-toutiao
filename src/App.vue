5bbc069092cafed8cb5e51f33974548db7fb8a3c
whatxin <2045513674@qq.com>
2019-07-10 21:17:20 +0800

    路由和登录页面的简单布局

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-<el-input-number v-model="num" @change="handleChange" :min="1" :max="10" label="描述文字"></el-input-number>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -10,12 +10,6 @@ export default {
   name: 'app',
   data () {
     return {
-      num: 1
-    }
-  },
-  methods: {
-    handleChange (value) {
-      console.log(value)
     }
   }
 }
