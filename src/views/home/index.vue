a83d1de6293af31d7c462f2c37b70799d52e7887
whatxin <2045513674@qq.com>
2019-07-12 01:10:36 +0800

    完成侧边栏布局和缩放

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,9 +1,51 @@
 <template>
     <el-container class="home-container">
-        <el-aside class="aside" width="200px">Aside</el-aside>
+        <el-aside class="aside" :width="togg?'64px':'200px'">
+            <div class="logo" :class="{xlogo:togg}"></div>
+
+        <el-menu
+        style="border-right:none"
+            default-active="1"
+            background-color="#002033"
+            text-color="#fff"
+            active-text-color="#ffd04b"
+            :collapse='togg'
+            :collapse-transition='false'
+            >
+            <el-menu-item index="1">
+                <i class="el-icon-s-home"></i>
+                <span slot="title">首页</span>
+            </el-menu-item>
+            <el-menu-item index="2">
+                <i class="el-icon-document"></i>
+                <span slot="title">内容管理</span>
+            </el-menu-item>
+            <el-menu-item index="3">
+                <i class="el-icon-picture"></i>
+                <span slot="title">素材管理</span>
+            </el-menu-item>
+            <el-menu-item index="4">
+                <i class="el-icon-s-promotion"></i>
+                <span slot="title">发布文章</span>
+            </el-menu-item>
+            <el-menu-item index="5">
+                <i class="el-icon-chat-dot-round"></i>
+                <span slot="title">评论管理</span>
+            </el-menu-item>
+            <el-menu-item index="6">
+                <i class="el-icon-present"></i>
+                <span slot="title">粉丝管理</span>
+            </el-menu-item>
+            <el-menu-item index="7">
+                <i class="el-icon-setting"></i>
+                <span slot="title">个人设置</span>
+            </el-menu-item>
+        </el-menu>
+
+        </el-aside>
         <el-container>
             <el-header class="header">
-                <span class="el-icon-s-fold"></span>
+                <span class="el-icon-s-fold" @click="toggleMenu()"></span>
                 <span class="text">江苏传智播客科技教育有限公司</span>
                 <el-dropdown style="float:right">
                     <span class="el-dropdown-link">
@@ -23,7 +65,16 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      togg: false
+    }
+  },
+  methods: {
+    toggleMenu () {
+      this.togg = !this.togg
+    }
+  }
 }
 </script>
 
@@ -36,6 +87,15 @@ export default {
   height: 100%;
   .aside{
       background: #002033;
+      .logo{
+          width: 100%;
+          height: 60px;
+          background: #024 url(../../assets/images/logo_admin.png) no-repeat center / 140px auto;
+      }
+      .xlogo{
+        background-image: url(../../assets/images/logo_admin_01.png);
+        background-size: 36px auto;
+      }
   }
   .header{
     border-bottom: 1px solid #ddd;
